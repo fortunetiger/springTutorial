06376763f60f611f7bd3cba102e716689ef7e90d
Inseo Song <songinseo0910@duksung.ac.kr>
2020-05-19 14:47:45 +0900

    add dataSource(hikariCP)

--- ex00/src/test/java/org/zerock/persistence/DataSourceTest.java
+++ ex00/src/test/java/org/zerock/persistence/DataSourceTest.java
@@ -1,6 +1,11 @@
-package org.zerock.sample;
+package org.zerock.persistence;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,21 +19,18 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class SampleTest {
-	
-	@Setter(onMethod=@__({@Autowired}))
-	private Restaurant	restaurant;
-	private Chef		chef;
+public class DataSourceTest {
+
+	@Setter(onMethod_ = { @Autowired })
+	private DataSource dataSource;
 	
 	@Test
-	public void testExist(){
-		
-		assertNotNull(restaurant);
-		
-		log.info(restaurant);
-		log.info("------------");
-		
-		//call Restaurant object
-		log.info(restaurant.getChef());
+	public void testConnection() {
+		try ( Connection con = dataSource.getConnection() ){
+			log.info(con);
+		} catch( Exception e ) {
+			fail(e.getMessage());
+		}
 	}
+	
 }
